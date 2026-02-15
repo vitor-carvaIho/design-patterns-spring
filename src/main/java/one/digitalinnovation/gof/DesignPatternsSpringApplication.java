0bd83530f690a86826efe55432f93b627d1b4901
vitor-carvaIho <77708828+vitor-carvaIho@users.noreply.github.com>
2026-02-15 17:41:44 -0300

    Update DesignPatternsSpringApplication.java

--- src/main/java/one/digitalinnovation/gof/DesignPatternsSpringApplication.java
+++ src/main/java/one/digitalinnovation/gof/DesignPatternsSpringApplication.java
@@ -2,9 +2,10 @@ package one.digitalinnovation.gof;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient
+@EnableFeignClients
 @SpringBootApplication
 public class DesignPatternsSpringApplication {
 
